04c8420123187ecc41eb3397b2c697a0b2fadb88
Mark <marksmellie892@gmail.com>
2024-01-16 14:29:41 +0000

    rebuild

--- server/dist/index.js
+++ server/dist/index.js
@@ -10,7 +10,7 @@ const articles_1 = __importDefault(require("./routes/articles"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
-const bodyParser = require("body-parser");
+const body_parser_1 = __importDefault(require("body-parser"));
 const port = process.env.PORT || 8080;
 dotenv_1.default.config();
 mongoose_1.default
@@ -18,26 +18,25 @@ mongoose_1.default
     .then(() => {
     console.log("Connected to DB!");
     const app = (0, express_1.default)();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.get("/", (req, res) => {
-        res.json("Hello");
-    });
-    app.use("/", (req, res) => {
-        res.send("Server is running.");
-    });
-    app.listen(port, () => {
-        console.log(`Now listening to port ${port}`);
-    });
+    app.use(body_parser_1.default.json());
+    app.use(body_parser_1.default.urlencoded({ extended: false }));
     app.use(express_1.default.json());
     app.use((0, cors_1.default)({
         origin: ["https://subscription-app-eight.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true,
     }));
+    // Define your routes
     app.use("/auth", auth_1.default);
     app.use("/subs", subs_1.default);
     app.use("/articles", articles_1.default);
+    // Default route
+    app.get("/", (req, res) => {
+        res.json("Hello");
+    });
+    app.listen(port, () => {
+        console.log(`Now listening to port ${port}`);
+    });
 })
     .catch((error) => {
     console.log({ error });
